852b7c6e23bc2c04bf542b29107f02933f0f26f0
Kateryna0712 <68386513+Kateryna0712@users.noreply.github.com>
2020-09-05 17:50:23 +0200

    Update Main.java

--- lesson_06_homework/src/com/lgs/lab/interface2/Main.java
+++ lesson_06_homework/src/com/lgs/lab/interface2/Main.java
@@ -4,10 +4,10 @@ import com.lgs.lab.interface1.Numerable;
 
 public class Main {
 public static void main(String[] args) {
-	MyCalculator calculator = new MyCalculator(67, 23);
-	calculator.ViewResult(calculator.plus());
-	calculator.ViewResult(calculator.minus());
-	calculator.ViewResult(calculator.multiplay());
-	calculator.ViewResult(calculator.devide());
-}
+	 MyCalculator calculator=new MyCalculator();
+     System.out.println(calculator.plus(23,67));
+     System.out.println(calculator.minus(127,5));
+     System.out.println(calculator.devide(600,3));
+     System.out.println(calculator.multiplay(45,89));
+ }
 }
