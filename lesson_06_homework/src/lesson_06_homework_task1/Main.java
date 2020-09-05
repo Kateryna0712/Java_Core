3d9fb18a0cbd1fb391d3643a75e7094f9b236533
Kateryna0712 <68386513+Kateryna0712@users.noreply.github.com>
2020-09-05 17:57:53 +0200

    Update Main.java

--- lesson_06_homework/src/lesson_06_homework_task1/Main.java
+++ lesson_06_homework/src/lesson_06_homework_task1/Main.java
@@ -3,10 +3,11 @@ package lesson_06_homework_task1;
 public class Main {
 	public static void main(String[] args) {
 
-		HourlyRateWorker worker1 = new HourlyRateWorker("John", 168, 12);
-		FixedSalaryWorker worker2 = new FixedSalaryWorker("Sarah", 2500);
+		HourlyRateWorker worker1 = new HourlyRateWorker("jgguy", 172, 12);
 		worker1.salary();
+		FixedSalaryWorker worker2 = new FixedSalaryWorker("Sarah", 2500);
 		worker2.salary();
+
 	}
 
 }
